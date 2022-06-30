be9e0802188be3b0adaa9f88af1e174b52f61109
RafaRamosCosta <rm94281@fiap.com.br>
2022-06-30 17:00:33 -0300

    exercício com array, objeto e função.

---
+++ Section2/exercicios/exercicio-final-basico/js/script.js
@@ -0,0 +1,28 @@
+function meuEscopo() {
+  const form = document.querySelector("form");
+  const resultado = document.querySelector(".resultado");
+
+  const pessoas = [];
+
+  function recebeEventoForm(e) {
+    e.preventDefault();
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const peso = form.querySelector(".peso");
+    const altura = form.querySelector(".altura");
+    
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+    });
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value}<p>` + `<p>${peso.value}kg, ${altura.value}m`
+
+  }
+
+  form.addEventListener("submit", recebeEventoForm);
+}
+meuEscopo();
