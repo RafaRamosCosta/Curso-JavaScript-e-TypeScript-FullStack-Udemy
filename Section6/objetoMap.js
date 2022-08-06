c7194b4b870737eac4ee983923c19b4e121877a8
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-06 19:02:16 -0300

    Entendendo o objeto Map.

---
+++ Section6/objetoMap.js
@@ -0,0 +1,24 @@
+const pessoas = [
+    { id: 3, nome: 'Rafa'},
+    { id: 2, nome: 'Ana'},
+    { id: 1, nome: 'Bia'}
+];
+
+// dessa forma a ordem não é mantida
+// const novasPessoas = {};
+// for (const pessoa of pessoas) {
+//     const { id } = pessoa;
+//     novasPessoas[id] = { ...pessoa };
+// }
+
+// dessa forma a orderm é mantida
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+    const { id } = pessoa;
+    novasPessoas.set(id, { ...pessoa });
+}
+
+// retorna um array com o id e o objeto 
+for (const pessoa of novasPessoas) {
+    console.log(pessoa);
+}
