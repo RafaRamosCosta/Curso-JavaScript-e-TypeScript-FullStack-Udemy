2c3d09d0aaf7ba76a9198f2ad871de0d405fa6c7
RafaRamosCosta <rm94281@fiap.com.br>
2022-09-20 19:02:57 -0300

    Entendendo as funcionalidades do fs.

---
+++ Section10/aula3/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+// listando arquivos com fs e recursão mútua
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir);
+    walk(files, rootDir);
+}
+
+async function walk(files, rootDir) {
+    for(let file of files) {
+        const fileFullPath = path.resolve(rootDir, file);
+        const stats = await fs.stat(fileFullPath);
+
+        if(/\.git/g.test(fileFullPath)) continue;
+        if(/\.node_modules/g.test(fileFullPath)) continue;
+        if(stats.isDirectory()) {
+            readdir(fileFullPath);
+            continue;
+        }
+        
+        if(!/\.css/g.test(fileFullPath)) continue;
+        console.log(file, stats.isDirectory());
+    }
+}
+
+readdir('C:/projects/Udemy/');
