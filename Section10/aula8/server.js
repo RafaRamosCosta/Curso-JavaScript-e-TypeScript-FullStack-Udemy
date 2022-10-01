492b58aeb64b20e163e89e78a5ad7753728da80b
RafaRamosCosta <rm94281@fiap.com.br>
2022-10-01 15:22:31 -0300

    entendendo sobre o body, parametros e querys da requisição.

---
+++ Section10/aula8/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+// função para acessar o body da requisição
+app.use(express.urlencoded({ extended: true }));
+// http://site.com/rota/parametro?query=valor&query2=valor2
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action='/' method='POST'>
+        Nome: <input type='text' name='name'>
+        Sobrenome: <input type='text' name='sobrenome'>
+        <button>Enviar</button>
+    </form>`);
+});
+
+app.get('/tests/:idUsuarios?/:parametro?', (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send(req.params);
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`Recebi o formulário. Nome: ${req.body.name} ${req.body.sobrenome}`);
+});
+
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato conosco!');
+});
+
+app.listen(3000, () => {
+  console.log('Acesse: http://localhost:3000');
+  console.log('Server is running on port 3000');
+});
