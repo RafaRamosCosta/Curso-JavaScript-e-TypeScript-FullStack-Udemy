0f093abb107e6f69bb60097592289ca650f58733
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-18 17:26:26 -0300

    Parte do servidor configurada com o banco via mongoose. Rotas criadas.

---
+++ Section10/projeto-agenda/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
+// rotas da home
+// middleware
+route.get('/', homeController.index);
+
+// rotas de login
+route.get('/login', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+// rotas de contato
+route.get('/contato', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
+// exportando todas as rotas que serão criadas
+module.exports = route;
---
+++ Section10/projeto-agenda/server.js
@@ -0,0 +1,78 @@
+// gerencia informções privadas como senha
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const path = require('path');
+const helmet = require('helmet');
+const csurf = require('csurf');
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require('./src/middlewares/middleware');
+
+app.use(helmet());
+
+// realiza o parse pra json na aplicação
+app.use(express.json());
+
+
+// método para acessar o body da requisição
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    app.emit('conectado');
+  })
+  .catch((e) => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+// setando as opções de sessão
+const sessionOptions = session({
+  secret: 'adshgjadskif1345324jas03-9543jl 132ffgs6902çásaáfaaf',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias em milisegundos
+    httpOnly: true,
+  },
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
+// método para definir o conteúdo estático da aplicação
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// método para setar as views da aplicação
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(csurf());
+
+// métodos para utilizar os middlewares
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+
+// método para acessar as rotas da aplicação
+app.use(routes);
+
+app.on('conectado', () => {
+  app.listen(3000, () => {
+    console.log('Acesse: http://localhost:3000');
+    console.log('Server is running on port 3000');
+  });
+});
